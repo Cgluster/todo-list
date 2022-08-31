018f9467f66eebceceeddaf2a8744d9a1ab039c9
unknown <luster.clifford@yahoo.com>
2022-08-31 15:03:15 -0700

    fourth commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myList = document.getElementsByTagName('li');
+const myList = document.getElementsByTagName('LI');
 for (let i = 0; i < myList.length; i++) {
     const span = document.createElement("SPAN");
     const text = document.createTextNode('\u00D7');
@@ -7,7 +7,7 @@ for (let i = 0; i < myList.length; i++) {
     myList[i].appendChild(span);
 }
 
-const close = getElementsByClassName('close');
+const close = document.getElementsByClassName('close');
 for (let i = 0; i < close.length; i++) {
     close[i].onclick = function() {
         const div = this.parentElement;
@@ -15,15 +15,15 @@ for (let i = 0; i < close.length; i++) {
     }
 }
 
-const list = document.querySelector('ul');
-list.addEventListener('click', function(e) {
-    if (e.target.tagName === 'li') {
-        e.target.classList.toggle('checked');
+let list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
     }
 }, false);
 
 function addTask(){
-    const li = document.createElement('li');
+    const li = document.createElement('LI');
     const inputValue = document.getElementById('myInput').value;
     const t = document.createTextNode(inputValue);
     li.appendChild(t);
